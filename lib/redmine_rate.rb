c051bb44239878c911d8e41f0ce43b642257171a
Arthur Andersen <aandersen@velalu.qa>
2016-11-25 12:14:27 +0100

    Allow reporting time entries by billable state

--- lib/redmine_rate.rb
+++ lib/redmine_rate.rb
@@ -17,6 +17,7 @@ Rails.configuration.to_prepare do
   # Patches
   require 'redmine_rate/patches/time_entry_patch'
   require 'redmine_rate/patches/time_entry_query_patch'
+  require 'redmine_rate/patches/time_report_patch'
   require 'redmine_rate/patches/users_helper_patch'
 end
 
---
+++ lib/redmine_rate/patches/time_report_patch.rb
@@ -0,0 +1,34 @@
+require_dependency 'redmine/helpers/time_report'
+
+module RedmineRate
+  module Patches
+    module TimeReportPatch
+      def self.included(base)
+        base.extend(ClassMethods)
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+          alias_method_chain :load_available_criteria, :cost
+        end
+      end
+
+      module ClassMethods
+      end
+
+      module InstanceMethods
+        def load_available_criteria_with_cost
+          @available_criteria = load_available_criteria_without_cost
+          @available_criteria['billable'] = {
+            sql: "#{TimeEntry.table_name}.billable",
+            format: 'bool',
+            label: :billable
+          }
+          @available_criteria
+        end
+      end
+    end
+  end
+end
+
+unless Redmine::Helpers::TimeReport.included_modules.include?(RedmineRate::Patches::TimeReportPatch)
+  Redmine::Helpers::TimeReport.send(:include, RedmineRate::Patches::TimeReportPatch)
+end
